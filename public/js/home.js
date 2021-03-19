e3e12aebe5fd5b0492472afa7a6dc2731eed610a
benwade91 <70988204+benn925@users.noreply.github.com>
2021-03-19 16:22:28 -0700

    updates to home page. added medium and mobile responsive sizes.

--- public/js/home.js
+++ public/js/home.js
@@ -24,9 +24,11 @@
 $('#loginBtn').click(function(){
     $('#loginForm').toggle();
     $('#signUpForm').css('display','none');
+    $('#carouselHero').css('display','none');
 });
 
 $('#signUpBtn').click(function(){
     $('#signUpForm').toggle();
     $('#loginForm').css('display','none');
+    $('#carouselHero').css('display','none');
 });
