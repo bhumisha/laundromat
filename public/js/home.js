56641beddb30bda9bc9e5b8b19ead7887d71f054
benwade91 <70988204+benn925@users.noreply.github.com>
2021-03-19 11:03:54 -0700

    changed homepage to login screen instead of ZIP search

--- public/js/home.js
+++ public/js/home.js
@@ -1,22 +1,32 @@
 
-// fetch('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=AIzaSyBz7e4X_piywmlaQOSdB4DPoaqq6PI9lfk')
+// fetch('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=API_KEY')
 
 //     .then(res => res.json())
 //     .then(data => {
 //         console.log(data);
 //     })
 
-$("#findZip").click(function () {
-    var searchFor = $("#zip-input").val();
-    console.log(searchFor);
-    $("#zip-input").val("");
+// $("#findZip").click(function () {
+//     var searchFor = $("#zip-input").val();
+//     console.log(searchFor);
+//     $("#zip-input").val("");
+// });
+
+// $("#zip-input").keypress(function (e) {
+//     var key = e.which;
+//     if (key == 13) {
+//         event.preventDefault();
+//         $("#findZip").click();
+//         $("#zip-input").val("");
+//     }
+// });
+
+$('#loginBtn').click(function(){
+    $('#loginForm').toggle();
+    $('#signUpForm').css('display','none');
 });
 
-$("#zip-input").keypress(function (e) {
-    var key = e.which;
-    if (key == 13) {
-        event.preventDefault();
-        $("#findZip").click();
-        $("#zip-input").val("");
-    }
+$('#signUpBtn').click(function(){
+    $('#signUpForm').toggle();
+    $('#loginForm').css('display','none');
 });
