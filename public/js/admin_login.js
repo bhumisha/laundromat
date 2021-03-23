06311cb6452ada2f9b356bd2dc522383c4cfd1f6
benwade91 <70988204+benn925@users.noreply.github.com>
2021-03-22 17:40:49 -0700

    connected signup/login input fields for modals

--- public/js/admin_login.js
+++ public/js/admin_login.js
@@ -1,28 +1,33 @@
 async function loginaFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('.loginAdminEmail').value.trim();
-    const password = document.querySelector('.loginAdminPw').value.trim();
-    
-    if (email && password) {
-      const response = await fetch('/api/admin/login', {
-        method: 'post',
-        body: JSON.stringify({
-            email,
-            password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/admin/orders/'); //IT will replace with ADmin - Laundromat's Orders.
-        //---------------REPLACED 'api/orders' with 'admin/orders'-----------------
-      } else {
-        alert(response.statusText);
+  event.preventDefault();
+
+  let email = document.querySelector('.loginAdminEmail').value.trim();
+  let password = document.querySelector('.loginAdminPw').value.trim();
+
+  if (!email) {
+    email = document.querySelector('.loginAdminEmailM').value.trim();
+    password = document.querySelector('.loginAdminPwM').value.trim();
+  }
+
+  if (email && password) {
+    const response = await fetch('/api/admin/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
       }
+    });
+
+    if (response.ok) {
+      document.location.replace('/admin/orders/'); //IT will replace with ADmin - Laundromat's Orders.
+      //---------------REPLACED 'api/orders' with 'admin/orders'-----------------
+    } else {
+      alert(response.statusText);
     }
   }
-  document.querySelector('.login-form').addEventListener('submit', loginaFormHandler);
-  
-  
-  
+}
+document.querySelector('.login-form').addEventListener('submit', loginaFormHandler);
+document.querySelector('.login-formM').addEventListener('submit', loginaFormHandler);
