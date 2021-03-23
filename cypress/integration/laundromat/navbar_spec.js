21125800a8333c808816f22f34c88be309b9e7e0
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-03-22 21:21:12 -0700

    adds cypress test framework

---
+++ cypress/integration/laundromat/navbar_spec.js
@@ -0,0 +1,25 @@
+describe('navbar tests', () => {
+  beforeEach(() => {
+    // '/' = baseUrl set in cypress.json - currently set to localhost:3000
+    cy.visit('/');
+  });
+
+  it('navbar-brand links to homepage', () => {
+    cy.log('Verify navbar-brand href');
+    cy.get('.navbar-brand').should('have.attr', 'href').and('eq', '/');
+  });
+
+  it('navbar login button opens login modal', () => {
+    cy.log('Login modal is not visible until navbar button clicked');
+    cy.get('#loginForm').should('not.be.visible');
+    cy.get('.navbar-nav > #loginBtn').click();
+    cy.get('#loginForm').should('be.visible');
+  });
+
+  it('navbar signup button opens signup modal', () => {
+    cy.log('Signup modal is not visible until navbar button clicked');
+    cy.get('#signUpForm').should('not.be.visible');
+    cy.get('.navbar-nav > #signUpBtn').click();
+    cy.get('#signUpForm').should('be.visible');
+  });
+});
