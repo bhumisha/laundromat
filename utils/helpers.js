ea300f15ad76b2a1ccb3597d8e42b8b94cd914d8
benwade91 <70988204+benn925@users.noreply.github.com>
2021-03-23 09:27:15 -0700

    added bag count to customer order & changed DATE field to only accept today or later

--- utils/helpers.js
+++ utils/helpers.js
@@ -22,5 +22,21 @@ module.exports = {
     }
 
     return word;
+  },
+
+  todays_date: () => {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd
+    }
+    if (mm < 10) {
+      mm = '0' + mm
+    }
+
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
   }
 };
