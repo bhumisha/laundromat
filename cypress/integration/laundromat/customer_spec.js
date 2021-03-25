f853dc10a2808589abd2011089b241ab5389104b
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-03-24 21:50:54 -0700

    adds tests for email uniqueness and incorrect password during login

--- cypress/integration/laundromat/customer_spec.js
+++ cypress/integration/laundromat/customer_spec.js
@@ -1,4 +1,6 @@
-import { newCustomer } from './util/customer';
+import { newCustomer } from './util/exports';
+
+const customer = newCustomer();
 
 describe('customer tests', () => {
   beforeEach(() => {
@@ -7,22 +9,11 @@ describe('customer tests', () => {
   });
 
   it('verifies new customer can sign up and place first order', () => {
-    const customer = newCustomer();
     const dateOfService = Cypress.moment().add(2, 'days').format('YYYY-MM-DD');
     const serviceDateConfirmation = Cypress.moment(dateOfService).format(
       'M/DD/YYYY'
     );
-    cy.log('Click Sign-Up button in hero');
-    cy.get('.d-flex > #signUpBtn').click();
-    cy.get('#signUpForm').should('be.visible');
-    cy.get('#signUpEmail').type(customer.email);
-    cy.get('#signUpPw').type(customer.password);
-    cy.get('#pwVerify').type(customer.password);
-    cy.get('#signUpStreetAdd').type(customer.street);
-    cy.get('#signUpCity').type(customer.city);
-    cy.get('#signUpState').select('CA');
-    cy.get('#signUpState').should('have.value', 'CA');
-    cy.get('#signUpZip').type(customer.zip);
+    fillInSignupForm(customer);
     cy.get('#signUpForm > .mt-3').click();
     cy.contains('Place Your Order', { matchCase: false });
     cy.url().should('include', '/cust');
@@ -33,17 +24,51 @@ describe('customer tests', () => {
     cy.get('#serviceSelect').select('Dry Clean');
     cy.get('#comment').type('Light starch');
     cy.get('.btn').contains('Confirm').click();
-
     //bug: the order confirmation shows the day before requested date of service.
     cy.get('.table-light > tr > :nth-child(1)').contains(
       '3/25/2021'
       // serviceDateConfirmation
     );
   });
-});
-//TODO: add test to verify email address uniqueness
 
-//TODO: Add test to login as user
-//login error states:
-//incorrect password
-//email id not found
+  it('verifies existing member member can login', () => {
+    cy.get('.navbar-nav > #loginBtn').click();
+    cy.get('#loginEmail').type(customer.email);
+    cy.get('#loginPw').type(customer.password);
+    cy.get('#loginForm > .mt-3').click();
+    cy.contains('Place your Order');
+  });
+
+  it('verifies incorrect password throws error', () => {
+    cy.get('.navbar-nav > #loginBtn').click();
+    cy.get('#loginEmail').type(customer.email);
+    cy.get('#loginPw').type('badPassword');
+    cy.get('#loginForm > .mt-3').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Unauthorized');
+    });
+  });
+
+  it('verifies user cannot signup with email address that has already been registered', () => {
+    fillInSignupForm(customer);
+    cy.get('#signUpForm > .mt-3').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Internal Server Error`);
+    });
+  });
+});
+
+/***** Helper Functions *****/
+function fillInSignupForm(newCustomer) {
+  cy.log('Click Sign-Up button in hero');
+  cy.get('.d-flex > #signUpBtn').click();
+  cy.get('#signUpForm').should('be.visible');
+  cy.get('#signUpEmail').type(newCustomer.email);
+  cy.get('#signUpPw').type(newCustomer.password);
+  cy.get('#pwVerify').type(newCustomer.password);
+  cy.get('#signUpStreetAdd').type(newCustomer.street);
+  cy.get('#signUpCity').type(newCustomer.city);
+  cy.get('#signUpState').select('CA');
+  cy.get('#signUpState').should('have.value', 'CA');
+  cy.get('#signUpZip').type(newCustomer.zip);
+}
